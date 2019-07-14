f72a36dd4ad391f6b910ca4dea16c8e0416d9918
Msheezi <marshall.randolph@gmail.com>
2019-07-13 18:47:40 -0700

    Fix User auth

--- app.js
+++ app.js
@@ -3,19 +3,31 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
-mongoose
-.connect(db,{useNewUrlParser:true})
-.then(()=> console.log("Connected to MongoDB successfully"))
-.catch(err=> console.log(err));
+const User = require('./models/User')
 const users = require("./routes/api/users")
 const tweets = require("./routes/api/tweets")
 const bodyParser = require('body-parser')
 
-app.get("/", (req,res)=>res.send("Hello World"));
+mongoose
+.connect(db,{useNewUrlParser:true})
+.then(()=> console.log("Connected to MongoDB successfully"))
+.catch(err=> console.log(err));
+
+app.get("/", (req,res)=>{
+    // console.log(res)  //This is here to trigger the node debugging console, make sure you are using the debug version of the server
+    // can also put a debugger here
+    // const user = new User({
+    //     handle: "jim",
+    //     email: "jimjhim",
+    //     password: "password"
+    // })
+    // user.save()
+    res.send("Hello World")});
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(bodyParser.json());
+
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
